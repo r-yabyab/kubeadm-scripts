e6744d2c45ce636bfe4d9ec7cbe6efea7386dfe1
Scruffy pc <cayabyabrr@yahoo.com>
2024-02-17 21:26:28 -0800

    dir before adding packages

--- scripts/common.sh
+++ scripts/common.sh
@@ -66,7 +66,8 @@ sudo sysctl --system
 sudo apt-get update
 sudo apt-get install -y software-properties-common curl
 
-sudo su
+# sudo su
+mkdir /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" |
@@ -76,7 +77,7 @@ curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /" |
     tee /etc/apt/sources.list.d/cri-o.list
-exit
+# exit
 
 sudo apt-get update
 sudo apt-get install -y cri-o kubelet kubeadm kubectl
