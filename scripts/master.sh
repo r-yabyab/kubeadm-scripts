417b5170661245c5215e86bb79d5ffad2cf971a7
Scruffy pc <cayabyabrr@yahoo.com>
2024-05-06 17:26:15 -0700

    master script

--- scripts/master.sh
+++ scripts/master.sh
@@ -11,6 +11,8 @@ NODENAME=$(hostname -s)
 # POD_CIDR="192.168.0.0/16"
 # FLANNEL_CIDR
 POD_CIDR="10.244.0.0/16"
+# CALICO_CIDR
+# POD_CIDR="192.168.0.0/16"
 
 # Pull required images
 
@@ -21,21 +23,21 @@ POD_CIDR="10.244.0.0/16"
 
 # Initialize kubeadm based on PUBLIC_IP_ACCESS
 
-if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
+# if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
     
-    MASTER_PRIVATE_IP=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
-    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+    # MASTER_PRIVATE_IP=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
+    sudo kubeadm init --apiserver-advertise-address="$local_ip" --apiserver-cert-extra-sans="$local_ip" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
     # sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr=10.244.0.0/16 --node-name "$NODENAME" --ignore-preflight-errors Swap
 
-elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
+# elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
 
-    MASTER_PUBLIC_IP=$(curl ifconfig.me && echo "")
-    sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
-    # sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors=Mem,Swap
-else
-    echo "Error: MASTER_PUBLIC_IP has an invalid value: $PUBLIC_IP_ACCESS"
-    exit 1
-fi
+#     MASTER_PUBLIC_IP=$(curl ifconfig.me && echo "")
+#     sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+#     # sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors=Mem,Swap
+# else
+#     echo "Error: MASTER_PUBLIC_IP has an invalid value: $PUBLIC_IP_ACCESS"
+#     exit 1
+# fi
 
 # Configure kubeconfig
 
@@ -48,9 +50,16 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 # # curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml -O
 # curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml -O
 
+# CALICO CNI 
+# documentation https://docs.tigera.io/calico/latest/getting-started/kubernetes/quickstart
 # kubectl apply -f calico.yaml
+# kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/tigera-operator.yaml
+# kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/custom-resources.yaml
+# kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+# kubectl taint nodes --all node-role.kubernetes.io/master-
 
-# 
+
+# FLANNEL CNI
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
 # then join workers
